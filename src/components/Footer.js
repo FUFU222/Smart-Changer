189b311c2d7581e20ebbc54224f50ac54951cf01
FUFU222 <tnkakr_0817@icloud.com>
2024-10-20 11:53:05 +0900

    style done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
           <li><Link to="/privacy-policy">プライバシーポリシー</Link></li>
           <li><Link to="/terms-of-service">利用規約</Link></li>
         </ul>
-        <p className="copyright">2024 A/dev</p>
+        <p className="copyright">Copyright © 2024 FUFU. All Rights Reserved.</p>
       </footer>
     </>
   )
